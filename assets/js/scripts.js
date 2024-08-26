1bf879208ca726833b02dc19e7952f14f008a809
toloespinosa <119706179+toloespinosa@users.noreply.github.com>
2024-08-26 19:40:20 -0400

    se suben los archivos

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+  
+    menuIcon.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+  });
+  
